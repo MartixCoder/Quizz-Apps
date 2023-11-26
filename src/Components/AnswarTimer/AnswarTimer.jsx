2f4c8dc65a72a6217afdda5783ed726ebf7bbe25
MartixCoder <martixcoder64@gmail.com>
2023-11-26 18:53:00 +0600

    mistake sloved

--- src/Components/AnswarTimer/AnswarTimer.jsx
+++ src/Components/AnswarTimer/AnswarTimer.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef} from "react";
-import "./AnswarTimer.scss";
+import "./answartimer.scss";
 
 const AnswarTimer = ({duration, onTimeUp}) => {
   const [counter, setCounter] = useState(0);
